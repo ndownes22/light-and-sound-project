5ec4239beb8ce1e1c0d227691319281275703389
Carlos Morales-Diaz <cmoralesdiaz@ufl.edu>
2021-03-30 23:52:51 -0400

    updated rtc tutorial

--- RTC_Tutorial/RTC_Tutorial.ino
+++ RTC_Tutorial/RTC_Tutorial.ino
@@ -19,18 +19,19 @@ void setup ()
     Serial.println("Couldn't find RTC");
     while (1);
   }
-
-  if (rtc.lostPower()) {
-    Serial.println("RTC lost power, lets set the time!");
-  
-  // Comment out below lines once you set the date & time.
-    // Following line sets the RTC to the date & time this sketch was compiled
     rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  
-    // Following line sets the RTC with an explicit date & time
-    // for example to set January 27 2017 at 12:56 you would call:
-    // rtc.adjust(DateTime(2017, 1, 27, 12, 56, 0));
-  }
+
+//  if (rtc.lostPower()) {
+//    Serial.println("RTC lost power, lets set the time!");
+//  
+//  // Comment out below lines once you set the date & time.
+//    // Following line sets the RTC to the date & time this sketch was compiled
+//    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+//  
+//    // Following line sets the RTC with an explicit date & time
+//    // for example to set January 27 2017 at 12:56 you would call:
+//    // rtc.adjust(DateTime(2017, 1, 27, 12, 56, 0));
+//  }
 }
 
 void loop () 
