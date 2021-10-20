31ebebe95e39c6593c92f73cdac0e7f08c552f95
Carlos-Morales-Diaz <43348653+Carlos-Morales-Diaz@users.noreply.github.com>
2021-10-20 06:03:12 -0400

    Update Light_And_Sound.ino
    Cleaned up the code for Fall 2021. Updated pin connections. We're using the Sparkfun Sound Detector's Envelope. Saving values to SD card needs it's own function call (commented).

--- Light_And_Sound/Light_And_Sound.ino
+++ Light_And_Sound/Light_And_Sound.ino
@@ -27,34 +27,28 @@
  * GND -> GND
  * ------------------
  * ---- Sound Sensor -----
- * A0 -> A0 (Analog)
+ * Envelope -> A0 (Analog)
  * Vin -> 3.3-5.5V DC
  * GND -> GND
+ * The Gate output is a binary indication 
+ * that is high when sound is present, and 
+ * low when conditions are quiet.
  * ------------------ 
  * 
  */
 
-#include <Wire.h> // Needed for I2C (SDA,SCL)
-#include <LiquidCrystal_I2C.h> // LCD library
-LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-#include <SPI.h> // Needed for the microSD card module
-#include <SD.h> // SD card library
+#include <Wire.h>                 // Needed for I2C (SDA,SCL)
+#include <LiquidCrystal_I2C.h>    // LCD library
+LiquidCrystal_I2C lcd(0x27,20,4); // set the LCD address to 0x27 for a 16 chars and 2 line display
+#include <SPI.h>                  // Needed for the microSD card module
+#include <SD.h>
+#include <Adafruit_Sensor.h>      // Lux sensor
+#include "Adafruit_TSL2591.h"     // Lux sensor
 #include "RTClib.h"
-#include <Adafruit_Sensor.h>
-#include "Adafruit_TSL2591.h"
-
-#define PIN_GATE_IN 2
-#define IRQ_GATE_IN  0
-#define PIN_LED_OUT 9
-#define PIN_ANALOG_IN A0
+#define PIN_ANALOG_IN A0 
 
 RTC_DS3231 rtc;
-//char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-
-Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // pass in a number for the sensor identifier (for your use later)
-
-//#define soundSensor A0
-//#define lightSensor A1 Lux sensor uses library function to calculate lux from ir and full
+Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // Lux sensor identifier
 File myFile;
 
 /*********************************************************/
@@ -72,13 +66,7 @@ File myFile;
 //  myFile.println(sound); 
 //  myFile.close();
 //}
-//void soundISR()
-//{
-//  int pin_val;
-//
-//  pin_val = digitalRead(PIN_GATE_IN);
-//  digitalWrite(PIN_LED_OUT, pin_val);   
-//}
+
 /***************** Configure Lux Sensor **************************/
 void configureSensor(void)
 {
@@ -122,109 +110,57 @@ void configureSensor(void)
   Serial.println(F(""));
 }
 
+void printError(String error) {
+  // We will reserve the fourth row of the LCD for any errors
+  lcd.setCursor(0,3);
+  lcd.print("Error: " + error);
+}
 
 void setup()
 {
   Serial.begin(9600);  
-
   // --------------- Set up the Lux Sensor ------------------
-  Serial.println(F("Starting Adafruit TSL2591 Test!"));
-  if (tsl.begin()) 
-  {
-    Serial.println(F("Found a TSL2591 sensor"));
-  } 
-  else 
-  {
-    Serial.println(F("No sensor found ... check your wiring?"));
-    while (1);
-  }
-  /* Configure the sensor */
+  if (!tsl.begin()) 
+    printError("Lux Sensor");
   configureSensor();
   //--------------------------------------------------
-  // Sound Sensor using Gate and Envelope
-  //  Configure LED pin as output
-//  pinMode(PIN_LED_OUT, OUTPUT);
-
-  // configure input to interrupt
-  pinMode(PIN_GATE_IN, INPUT);
-//  attachInterrupt(IRQ_GATE_IN, soundISR, CHANGE);
-
-  // Display status
-  Serial.println("Initialized");
-  //
 
   // --------------- Set up the LCD ------------------
-  lcd.init();  //initialize the lcd
-  lcd.backlight();  //open the backlight 
-  
-  lcd.setCursor ( 0, 0 );              // go to the top left corner
-  lcd.print("Date: "); // write this string on the top row
-  lcd.setCursor ( 0, 1 );              // go to the 2nd row
-  lcd.print("Light: ");   // pad string with spaces for centering
-  lcd.setCursor ( 0, 2 );              // go to the third row
-  lcd.print("Sound: ");   // pad with spaces for centering
-//lcd.setCursor ( 0, 3 );            // go to the fourth row
-//lcd.print("Last Row");
+  lcd.init();             // Initialize the lcd
+  lcd.backlight();        // Open the backlight 
+  lcd.setCursor ( 0, 0 ); // Go to the top left corner
+  lcd.print("Date: ");    // Write this string on the top row
+  lcd.setCursor ( 0, 1 );
+  lcd.print("Light: ");
+  lcd.setCursor ( 0, 2 );
+  lcd.print("Sound: ");
   //--------------------------------------------------
 
 
   // --------------- Set up the RTC ------------------
-  if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    while (1);
-  }
+  if (!rtc.begin())
+    printError("Couldn't find RTC");
   rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-//  if (rtc.lostPower()) {
-//    Serial.println("RTC lost power, lets set the time!");
-//    // Following line sets the RTC to the date & time this sketch was compiled
-//    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-//  }
+  //  if (rtc.lostPower()) {
+  //    Serial.println("RTC lost power, lets set the time!");
+  //    // Following line sets the RTC to the date & time this sketch was compiled
+  //    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  //  }
   //--------------------------------------------------
- 
-  Serial.print("Initializing SD card...");
-  if (!SD.begin(10)) {
-  Serial.println("initialization failed!");
-  while (1);
-  }
-  Serial.println("initialization done.");
-  
-  // as far as my understanding goes, this will only be needed if the clock is completely disconnected from power
-  // so, this should be run on the very first usage of the clock but should not be needed again
-  if (!rtc.begin()) { //(!rtc.initialized()) {
-      // following line sets the RTC to the date & time this sketch was compiled
-      rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); //example: to set January 27 2017 at 12:56, input rtc.adjust(DateTime(2017, 1, 27, 12, 56, 0));
-      
-//  myFile = SD.open("values_rtc.csv", FILE_WRITE);
-//  DateTime rightNow = rtc.now();
-//  myFile.print(rightNow.year(), DEC);
-//  myFile.print('/');
-//  myFile.print(rightNow.month(), DEC);
-//  myFile.print('/');
-//  myFile.println(rightNow.day(), DEC);
-//  myFile.close();
-  }
-}
 
-///***************** Lux Sensor Read Config **************************/
-//void advancedRead(void)
-//{
-//  // More advanced data read example. Read 32 bits with top 16 bits IR, bottom 16 bits full spectrum
-//  // That way you can do whatever math and comparisons you want!
-//  uint32_t lum = tsl.getFullLuminosity();
-//  uint16_t ir, full;
-//  ir = lum >> 16;
-//  full = lum & 0xFFFF;
-//  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
-//  Serial.print(F("IR: ")); Serial.print(ir);  Serial.print(F("  "));
-//  Serial.print(F("Full: ")); Serial.print(full); Serial.print(F("  "));
-//  Serial.print(F("Visible: ")); Serial.print(full - ir); Serial.print(F("  "));
-//  Serial.print(F("Lux: ")); Serial.println(tsl.calculateLux(full, ir), 6);
-//}
+  
+/***************** Configure MicroSD Card **************************/
+  if (!SD.begin(10)) // This begins use of the SPI bus. Parameter is CS pin 10
+    printError("SD Card Initialization Failed!"); 
+  myFile = SD.open("values.txt", FILE_WRITE);
+  if (!myFile)
+    printError("SD Card Open");
+/******************************************************************/
+
+}
 
 void loop() 
 {
-
-  rtc.begin();
   DateTime rightNow = rtc.now();
   String year_ = String(rightNow.year());
   String month_ = String(rightNow.month());
@@ -235,60 +171,39 @@ void loop()
   DateTime one_day (rightNow + TimeSpan(1,0,0,0)); //1 day 0 hr 0 min 0sec
 //  save = rtc.setAlarm1(one_day, DS3231_A1_Day);
 
-// Sound Sensor
+// Sound sensor
   int soundLevel;
-  // Check the envelope input
   soundLevel = analogRead(PIN_ANALOG_IN);
 
-  // Convert envelope value into a message
-  Serial.print("Status: ");
-  Serial.println(soundLevel);
-  
-
-//  soundLevel = analogRead(soundSensor);
-
-  // More advanced data read example. Read 32 bits with top 16 bits IR, bottom 16 bits full spectrum
+  // Lux sensor
+  // Read 32 bits with top 16 bits IR, bottom 16 bits full spectrum
   uint32_t lum = tsl.getFullLuminosity();
   uint16_t ir, full;
   ir = lum >> 16;
   full = lum & 0xFFFF;
-  Serial.print(F("[ ")); Serial.print(millis()); Serial.print(F(" ms ] "));
-  Serial.print(F("IR: ")); Serial.print(ir);  Serial.print(F("  "));
-  Serial.print(F("Full: ")); Serial.print(full); Serial.print(F("  "));
-  Serial.print(F("Visible: ")); Serial.print(full - ir); Serial.print(F("  "));
-  Serial.print(F("Lux: ")); Serial.println(tsl.calculateLux(full, ir), 6);
   int lux = tsl.calculateLux(full, ir);
 
-//  advancedRead();
-//  delay(500);
-  
-//  delay(1000); //1 sec
-//  delay(1000); //1 sec
-//  delay(1000); //1 sec
-
+  // Print to LCD screen
   lcd.clear();
-
-  lcd.setCursor ( 0, 0 );            // go to the top left corner
-  lcd.print("Date: " ); // write this string on the top row
-  lcd.print(rightNow.hour(), DEC);
+  lcd.setCursor(0,0);   // Go to the top left corner
+  lcd.print("Date: ");  // Write this string on the top row
+  lcd.print(rightNow.month(), DEC);
   lcd.print('/');
-  lcd.print(rightNow.minute(), DEC);
+  lcd.print(rightNow.day(), DEC);
   lcd.print('/');
-  lcd.print(rightNow.second(), DEC);
+  lcd.print(rightNow.year(), DEC);
       
-  lcd.setCursor ( 0, 1 );            // go to the 2nd row
-  lcd.print("Light Level: "); // pad string with spaces for centering
+  lcd.setCursor (0,1);
+  lcd.print("Light Level: ");
   lcd.print(lux);
   lcd.print(" Lux"); 
   
-  lcd.setCursor ( 0, 2 );            // go to the third row
-  lcd.print("Sound Level: "); // pad with spaces for centering
+  lcd.setCursor (0,2);
+  lcd.print("Sound Level: ");
   lcd.print(soundLevel); 
 
 
 //  save_values(lux, soundLevel);
-
-  myFile = SD.open("values.csv", FILE_WRITE);
   myFile.print(rightNow.hour(), DEC);
   myFile.print(':');
   myFile.print(rightNow.minute(), DEC);
@@ -299,6 +214,11 @@ void loop()
   myFile.print(","); 
   myFile.println(soundLevel); 
   myFile.close();
+
+  
+  // Print to serial monitor (for debugging)
+  Serial.print("Status: ");
+  Serial.println(soundLevel);
   
   delay(500); //half a second
 
